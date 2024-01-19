9a4059c7b59e7b394836a9118b29f8767817dadf
Rizul Gupta <mail2rizul@gmail.com>
2024-01-19 21:11:57 +0530

    modify formatting
    Signed-off-by: Rizul Gupta <mail2rizul@gmail.com>

--- Ques-1/Q_1Deque.cpp
+++ Ques-1/Q_1Deque.cpp
@@ -1,90 +1,33 @@
-class Node {
-public:
-    int data;
-    Node* next;
-    Node* prev;
-
-    Node(int value) {
-        data = value;
-        next = nullptr;
-        prev = nullptr;
-    }
-};
-
 class Deque {
 private:
-    Node* front;
-    Node* rear;
+    LinkedList list;
 
 public:
-    Deque() {
-        front = nullptr;
-        rear = nullptr;
-    }
-
     void push_front(int num) {
-        Node* new_node = new Node(num);
-        if (!front) {
-            front = rear = new_node;
-        } else {
-            new_node->next = front;
-            front->prev = new_node;
-            front = new_node;
-        }
+        list.insert_at_start(num);
     }
 
     void pop_front() {
-        if (front) {
-            Node* temp = front;
-            front = front->next;
-            if (front) {
-                front->prev = nullptr;
-            } else {
-                rear = nullptr;
-            }
-            delete temp;
-        }
+        list.remove_from_start();
     }
 
     void push_back(int num) {
-        Node* new_node = new Node(num);
-        if (!rear) {
-            front = rear = new_node;
-        } else {
-            new_node->prev = rear;
-            rear->next = new_node;
-            rear = new_node;
-        }
+        list.insert_at_end(num);
     }
 
     void pop_back() {
-        if (rear) {
-            Node* temp = rear;
-            rear = rear->prev;
-            if (rear) {
-                rear->next = nullptr;
-            } else {
-                front = nullptr;
-            }
-            delete temp;
-        }
+        list.remove_from_end();
     }
 
-    int front_() {
-        return front ? front->data : -1;
+    int front() {
+        return list.front();
     }
 
     int back() {
-        return rear ? rear->data : -1;
+        return list.last();
     }
 
     void traverse() {
-        Node* temp = front;
-        while (temp) {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-        cout <<endl;
+        list.traverse();
     }
 };
-
